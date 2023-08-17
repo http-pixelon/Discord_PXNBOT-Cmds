dd9fb3d0bd99b6117ef9695c60adac5c5ad26616
Kauã Frigieri <41579326+http-pixelon@users.noreply.github.com>
2023-08-17 16:44:19 -0300

    Update ranklibras.js

--- scr/functions_slw/commands/Economy-Pxn/ranklibras.js
+++ scr/functions_slw/commands/Economy-Pxn/ranklibras.js
@@ -8,7 +8,7 @@ module.exports = {
   description: "SlowPixels | Cmds_Economia: Veja o top melhores de money no teu servidor!",
   usage: '.ranklibras',
   category: 'economia-k33',
-  valor: '007
+  valor: '007',
       
 
   run: async (client, message, args) => {
